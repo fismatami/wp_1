43d2e68eaabd9da00d7d954ae4eb3f792ac3dd0f
Fismatami <avilafismatami@gmail.com>
2020-03-02 10:45:27 +0700

    membuat fitur login

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+	<h1 align="center">Welcome to Administrator</h1>
+	<form method="post" action="proses_login.php" align="center"> 
+		Username <input type="text" name="username"><br> 
+		Password <input type="password" name="password"><br>
+		<br>
+		<button type="submit", name="submit" value="">submit</button>
+	</form>
+
+</body>
+</html>
